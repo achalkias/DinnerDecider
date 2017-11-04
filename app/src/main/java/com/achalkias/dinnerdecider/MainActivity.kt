f601b354227dbccaf08d52b2a8cd53f6f647094f
achalkias <achalkias@hotmail.com>
2017-11-04 14:08:45 +0200

    Functionality created

--- app/src/main/java/com/achalkias/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/achalkias/dinnerdecider/MainActivity.kt
@@ -2,12 +2,32 @@ package com.achalkias.dinnerdecider
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    val foodList = arrayListOf("Chinese", "Hamburger", "Pizza", "McDonalds", "Barros Pizza")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+
+    }
+
+    private fun setListeners() {
+        decideBtn.setOnClickListener {
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            selectedFootTxt.text = foodList[randomFood]
+        }
+
+        addFoodBtn.setOnClickListener {
+            val newFood = addFoodText.text.toString()
+            foodList.add(newFood)
+            addFoodText.text.clear()
+            println(foodList)
+        }
     }
 }
